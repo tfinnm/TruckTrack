c4a856083926ffac93572ba5a5a1ea0ddf635bde
Toby McDonald <toby_mcdonald@email.com>
2023-09-23 22:50:44 -0400

    Field Client now knows who it is

--- field.php
+++ field.php
@@ -1,3 +1,15 @@
+<?php
+if (isset($_GET["t"])) {
+	include("db.php");
+	$conn = new mysqli($db_server, $db_user, $db_password, $db_db);
+	if ($conn->connect_error) {
+		// connection failed error goes here
+	}
+	$sql = "SELECT * FROM trucks WHERE UTID = '".$_GET["t"]."';";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+?> 
 <link rel="stylesheet" href="libraries/bootstrap-3.4.1-dist/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="libraries/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
@@ -13,13 +25,13 @@
 			<button onClick="sendStatus('reststop')" type="button" class="btn btn-primary">Signal Rest Stop</button><br><br>
 			<button onClick="sendStatus('traffic')" type="button" class="btn btn-warning">Signal Heavy Traffic</button><br><br>
 			<button onClick="sendStatus('breakdown')" type="button" class="btn btn-danger">Signal Breakdown</button><br><br>
-			<p style="position: fixed; bottom: 10;">Reporting as: A-59-39</p>
+			<p style="position: fixed; bottom: 10;">Reporting as: <?php echo $row["Name"]; ?></p>
 		</center>
 	</div>
 	<div style='height: 100%' class='col-sm-9' id='truckmap'></div>
 </div>
 <script>
-	var mymap = L.map('truckmap').setView(new L.LatLng(0, 0), 0);
+	var mymap = L.map('truckmap').setView(new L.LatLng(<?php echo $row["Lat"]; ?>, <?php echo $row["Lon"]; ?>), 4);
 	L.control.scale().addTo(mymap);
 	L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: 'Tiles &copy; Open Street Map'}).addTo(mymap);
 	var markerGroup = L.layerGroup().addTo(mymap);
@@ -33,13 +45,13 @@
 		var ajax = new XMLHttpRequest();
 		ajax.open('POST', 'sendloc.php');
 		ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-		ajax.send('lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&truck=1');
+		ajax.send('lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&truck=<?php echo $_GET["t"]; ?>');
 	}, (error) => {}, options);
 	function sendStatus(status) {
 		var ajax = new XMLHttpRequest();
 		ajax.open('POST', 'updatestat.php');
 		ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-		ajax.send('status='+status+'&truck=1');
+		ajax.send('status='+status+'&truck=<?php echo $_GET["t"]; ?>');
 	}
 	if(typeof(EventSource) !== "undefined") {
 	var sourcemap = new EventSource("updateMap.php");
@@ -49,3 +61,12 @@
 	};
 }
 </script>
+<?php
+	}
+} else {
+	echo "Truck Not Found";
+}
+} else {
+	echo "Truck Not Specified";
+}
+?>
